733d0666880ca480eb8b18e1af2299d222acd0e6
Bruno Ferreira <fbruno233@gmail.com>
2024-01-02 21:39:41 -0300

    dinamically and statically

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -11,15 +11,25 @@ contract Storage {
     //keccak256(key + 0x2)
     mapping (uint => uint) storingMap; //0x2
 
+    uint256 z = 20; //0x3
+
+    bool b = true; //0x4 package
+    uint8 public c = 0xff; //0x4 package with b; whatever public ou não
+
+    function getB() external view returns(bool){
+       return b;
+    }
+
+    uint256[3] s = [10, 22, 303];
+    uint256[] v;
+
     constructor() {
         //keccak256(0 + 0x2)
         storingMap[0] = 1;
         //keccak256(1 + 0x2)
         storingMap[1] = 2;
+
+        v.push(10);
+        v.push(0xf1092);
     }
-
-    uint256 z = 20; //0x3
-
-    bool b = true; //0x4 package
-    uint8 c = 0xff; //0x4 package with b
 }
--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const { keccak256 } = hre.ethers
 
-const addr = '0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0'
+const addr = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
 
 async function lookup() {
   const valueX = await hre.ethers.provider.getStorage(addr, '0x1')
@@ -19,16 +19,36 @@ async function lookup() {
   const valueM = await hre.ethers.provider.getStorage(addr, slot)
 
   console.log(parseInt(valueM))
+
   console.log("############## Should be at position 0x2 or 0x3? #############################")
 
   const valueZ = await hre.ethers.provider.getStorage(addr, '0x3')
 
   console.log(`${parseInt(valueZ)} 0x3!`)
+
   console.log("############## Package #############################")
-  const Package = await hre.ethers.provider.getStorage(addr, '0x4')
-  //const Package2 = await hre.ethers.provider.getStorage(addr, '0x5')
+  const Package = await hre.ethers.provider.getStorage(addr, '0x4') // B and C are package together in this slot
+  //const Package2 = await hre.ethers.provider.getStorage(addr, '0x5'); 
 
   console.log(`${parseInt(Package)}`)
+  const storage = await hre.ethers.getContractAt("Storage", addr);
+  let valueB = await storage.getB(); //Bitwise to separate B from C variable bits
+
+  console.log(`${valueB} valor de b`);
+
+  console.log("############## statically sized #############################")
+  const Statically = await hre.ethers.provider.getStorage(addr, '0x5') //0x6 e 0x7
+  console.log(`${parseInt(Statically)}`)
+
+  console.log("############## dynamically sized #############################")
+  const positionDynamicallyArray = hre.ethers.toBeHex(0x8, 2)
+  const dynamicallyArrayBase = hre.ethers.zeroPadValue(positionDynamicallyArray, 32); //Posição do array nos slots em hex
+  const dynamicallyValue = keccak256(dynamicallyArrayBase); //Posição zero do array
+
+  const nextP = BigInt(dynamicallyValue) + BigInt(1); //Para pegar alguma posição do array; dynamicallyValue + posição no array
+  
+  const dynamically = await hre.ethers.provider.getStorage(addr, '0x8') //0x8 (é a posição do array) Tamanho do array
+  console.log(`${parseInt(dynamically)}`)
 }
 
 lookup()
